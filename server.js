8180786ee8c12f8466133b88f49364c4bbc408cc
Nicholas Werner <ncwerner@gmail.com>
2021-02-02 08:33:21 -0700

    fixed error TravisCI found

--- server.js
+++ server.js
@@ -7,7 +7,6 @@ const session = require("express-session");
 const exphbs = require("express-handlebars");
 const client_id = process.env.CLIENT_ID; // Your client id
 const client_secret = process.env.CLIENT_SECRET; // Your secret
-const redirect_uri = "REDIRECT_URI"; // Your redirect uri
 
 // Requiring passport as we've configured it
 const passport = require("./config/passport");
